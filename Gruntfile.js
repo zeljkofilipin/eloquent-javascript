495f3beb4e6aa58c62b7565425900bcfd951660a
Željko Filipin <zeljko.filipin@gmail.com>
2017-01-03 10:56:53 +0100

    Set up Grunt
    $ npm install --save grunt load-grunt-tasks

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt)
+
+  // Project configuration
+  grunt.initConfig({
+  })
+
+  // Default tasks
+  grunt.registerTask('default', [])
+}
