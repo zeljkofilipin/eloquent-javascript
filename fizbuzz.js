a283249d0b279c97e606341fe53687c370bd1033
Željko Filipin <zeljko.filipin@gmail.com>
2017-01-05 12:57:22 +0100

    A simpler fizbuzz implementation

--- fizbuzz.js
+++ fizbuzz.js
@@ -1,13 +1,12 @@
 'use strict';
 
-for (let i=1; i<101; i++) {
-	if (i%3 === 0 && i%5 === 0) {
-		console.log('FizzBuzz');
-	} else if (i%3 === 0) {
-		console.log('Fizz');
-	} else if (i%5 === 0) {
-		console.log('Buzz');
-	} else {
-		console.log(i);
+for (let i = 1; i <= 100; i++) {
+	let output = '';
+	if (i % 3 === 0) {
+		output += 'Fizz';
 	}
+	if (i % 5 === 0) {
+		output += 'Buzz';
+	}
+	console.log(output || i);
 }
