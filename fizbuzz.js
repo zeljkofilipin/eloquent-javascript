3fe96d2dfc4f3299f343d34378bd0289ab3dbed9
Željko Filipin <zeljko.filipin@gmail.com>
2017-01-05 12:49:26 +0100

    Finished fizbuzz example

--- fizbuzz.js
+++ fizbuzz.js
@@ -1,6 +1,9 @@
 'use strict';
+
 for (let i=1; i<101; i++) {
-	if (i%3 === 0) {
+	if (i%3 === 0 && i%5 === 0) {
+		console.log('FizzBuzz');
+	} else if (i%3 === 0) {
 		console.log('Fizz');
 	} else if (i%5 === 0) {
 		console.log('Buzz');
