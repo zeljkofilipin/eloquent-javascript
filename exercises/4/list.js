ed89176a72cb0d6b695882da0a6d7350d0cb0129
Željko Filipin <zeljko.filipin@gmail.com>
2017-01-25 11:23:48 +0100

    4: listToArray

--- exercises/4/list.js
+++ exercises/4/list.js
@@ -13,4 +13,21 @@ function arrayToList(array) {
 	return list;
 }
 
+/**
+ * Convert list to array.
+ * @param {array} array The array.
+ * @param {list} list The list.
+ * @return {array} array The array.
+ */
+function listToArray(array, list) {
+	array.push(list.value);
+	if (list.rest) {
+		return listToArray(array, list.rest);
+	} else {
+		return array;
+	}
+}
+
 console.log(arrayToList([1, 2, 3]));
+let list = {value: 1, rest: {value: 2, rest: {value: 3, rest: null}}};
+console.log(listToArray([], list));
