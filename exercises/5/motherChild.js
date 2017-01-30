d74e42df41a353b3d5b2c8f021d24a9238ff1fb3
Željko Filipin <zeljko.filipin@gmail.com>
2017-01-30 12:57:18 +0100

    Update motherChild.js so ESLint is happy

--- exercises/5/motherChild.js
+++ exercises/5/motherChild.js
@@ -1,10 +1,26 @@
 // from http://eloquentjavascript.net/code/#5.2
+'use strict';
+
+/**
+ * Average.
+ * @param {array} array The array.
+ * @return {number} The average.
+ */
 function average(array) {
-  function plus(a, b) { return a + b; }
+  /**
+   * Plus.
+   * @param {number} a The first number.
+   * @param {number} b The second number.
+   * @return {number} The sum.
+   */
+  function plus(a, b) {
+    return a + b;
+  }
   return array.reduce(plus) / array.length;
 }
 
-var byName = {};
+let byName = {};
+let ancestry = require('../ancestry.js');
 ancestry.forEach(function(person) {
   byName[person.name] = person;
 });
