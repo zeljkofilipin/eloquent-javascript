65b7498e2fa94141143b528875c77950bce98df5
Željko Filipin <zeljko.filipin@gmail.com>
2017-01-30 12:40:34 +0100

    5: Mother-child age difference
    Code for the exercise copy/pasted from
    http://eloquentjavascript.net/code/ancestry.js
    and
    http://eloquentjavascript.net/code/#5.2

---
+++ exercises/5/motherChild.js
@@ -0,0 +1,14 @@
+// from http://eloquentjavascript.net/code/#5.2
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+// Your code here.
+
+// → 31.2
