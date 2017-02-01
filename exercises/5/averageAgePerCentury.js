1b25905469d56d9b3d2d8d09b211c588787d38c7
Željko Filipin <zeljko.filipin@gmail.com>
2017-02-01 12:24:56 +0100

    5: averageAgePerCentury()

--- exercises/5/averageAgePerCentury.js
+++ exercises/5/averageAgePerCentury.js
@@ -20,7 +20,61 @@ function average(array) {
 }
 
 // Your code here.
+let ancestry = require('../../code/5/ancestry.js');
 
+/**
+ * Age.
+ * @param {object} person Person object.
+ * @return {number} Age.
+ */
+function age(person) {
+	return person.died - person.born;
+}
+
+/**
+ * Century.
+ * @param {object} person Person object.
+ * @return {number} Century.
+ */
+function century(person) {
+	return Math.ceil(person.died/100);
+}
+
+let centuries = {
+	'16': [],
+	'17': [],
+	'18': [],
+	'19': [],
+	'20': [],
+	'21': [],
+};
+
+/**
+ * Age per century.
+ * @param {array} ancestry The array of objects.
+ */
+function agePerCentury(ancestry) {
+	ancestry.forEach(function(person) {
+		centuries[century(person)].push(age(person));
+	});
+}
+
+agePerCentury(ancestry);
+
+/**
+ * Average age per century.
+ * @param {object} agesByCentury Ages grouped by century.
+ * @return {object} Average age per century.
+ */
+function averageAgePerCentury(agesByCentury) {
+	let averages = {};
+	for( let i = 16; i < 22; i++) {
+		averages[i] = average(agesByCentury[i]);
+	}
+	return averages;
+}
+
+console.log(averageAgePerCentury(centuries));
 // → 16: 43.5
 //   17: 51.2
 //   18: 52.8
