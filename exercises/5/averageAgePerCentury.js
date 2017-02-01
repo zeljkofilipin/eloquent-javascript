e827c4d89f024f6bd3f57e0ac65fcb17b7ac8090
Željko Filipin <zeljko.filipin@gmail.com>
2017-02-01 11:24:27 +0100

    5: Historical life expectancy, copy/paste from http://eloquentjavascript.net/code/#5.3

---
+++ exercises/5/averageAgePerCentury.js
@@ -0,0 +1,14 @@
+// from http://eloquentjavascript.net/code/#5.3
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+// Your code here.
+
+// → 16: 43.5
+//   17: 51.2
+//   18: 52.8
+//   19: 54.8
+//   20: 84.7
+//   21: 94
