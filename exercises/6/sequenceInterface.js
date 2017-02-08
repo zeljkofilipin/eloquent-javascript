25ee6f916fb399a454d14b539aea39035deb72e8
Željko Filipin <zeljko.filipin@gmail.com>
2017-02-08 11:51:55 +0100

    6: Sequence interface: ArraySeq()

--- exercises/6/sequenceInterface.js
+++ exercises/6/sequenceInterface.js
@@ -13,10 +13,26 @@ function logFive(sequence) {
 	}
 }
 
+/**
+ * Sequentialize the array
+ * @param {array} array
+ */
+function ArraySeq(array) {
+  this.array = array;
+  this.i = -1;
+}
+ArraySeq.prototype.current = function() {
+  this.i++;
+  return this.array[this.i];
+};
+ArraySeq.prototype.next = function() {
+  return this.array[this.i + 1];
+};
+
 logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
-logFive(new RangeSeq(100, 1000));
+// logFive(new RangeSeq(100, 1000));
 // → 100
 // → 101
 // → 102
