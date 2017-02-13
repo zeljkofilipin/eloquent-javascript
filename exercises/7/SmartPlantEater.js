86985e8ff6edea06d06e8c836518a21293021edf
Željko Filipin <zeljko.filipin@gmail.com>
2017-02-13 16:50:48 +0100

    7: Artificial stupidity, more setup

--- exercises/7/SmartPlantEater.js
+++ exercises/7/SmartPlantEater.js
@@ -1,10 +1,15 @@
 // Your code here
+let LifelikeWorld = require('./code/07_elife.js').LifelikeWorld;
+let Wall = require('./code/07_elife.js').Wall;
+let PlantEater = require('./code/07_elife.js').PlantEater;
+let Plant = require('./code/07_elife.js').Plant;
+
 /**
  * Smart plant eater
  */
 function SmartPlantEater() {}
 
-animateWorld(new LifelikeWorld(
+let valley = new LifelikeWorld(
   ['############################',
    '#####                 ######',
    '##   ***                **##',
@@ -18,6 +23,13 @@ animateWorld(new LifelikeWorld(
    '##****     ###***       *###',
    '############################'],
   {'#': Wall,
-   'O': SmartPlantEater,
+   // 'O': SmartPlantEater,
+   'O': PlantEater,
    '*': Plant}
-));
+);
+
+for (let i = 0; i < 9; i++) {
+	valley.turn();
+	for (let j = 0; j < 999999999; j++) {}
+	console.log(valley.toString());
+}
