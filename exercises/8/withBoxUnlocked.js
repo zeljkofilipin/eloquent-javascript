2c883c7736efbf52ad99fd0f3c6cb15adfbd1351
Željko Filipin <zeljko.filipin@gmail.com>
2017-02-20 13:59:14 +0100

    8: The locked box, making eslint happy

--- exercises/8/withBoxUnlocked.js
+++ exercises/8/withBoxUnlocked.js
@@ -1,32 +1,42 @@
+'use strict';
+
 // from http://eloquentjavascript.net/code/chapter/08_error.js
 
-var box = {
+let box = {
   locked: true,
-  unlock: function() { this.locked = false; },
-  lock: function() { this.locked = true;  },
+  unlock: function() {
+    this.locked = false;
+  },
+  lock: function() {
+    this.locked = true;
+  },
   _content: [],
   get content() {
-    if (this.locked) throw new Error("Locked!");
+    if (this.locked) throw new Error('Locked!');
     return this._content;
-  }
+  },
 };
 
 // from http://eloquentjavascript.net/code/#8.2
 
+/**
+ * withBoxUnlocked
+ * @param {function} body
+ */
 function withBoxUnlocked(body) {
   // Your code here.
 }
 
 withBoxUnlocked(function() {
-  box.content.push("gold piece");
+  box.content.push('gold piece');
 });
 
 try {
   withBoxUnlocked(function() {
-    throw new Error("Pirates on the horizon! Abort!");
+    throw new Error('Pirates on the horizon! Abort!');
   });
 } catch (e) {
-  console.log("Error raised:", e);
+  console.log('Error raised:', e);
 }
 console.log(box.locked);
 // → true
