9183d660c4bd77263105f25149645211e3f27883
Željko Filipin <zeljko.filipin@gmail.com>
2017-02-21 12:32:30 +0100

    9: Regexp golf, eslinting

--- exercises/9/regexp_golf.js
+++ exercises/9/regexp_golf.js
@@ -1,44 +1,50 @@
+'use strict';
 // from http://eloquentjavascript.net/code/#9.1
 // Fill in the regular expressions
 
 verify(/.../,
-       ["my car", "bad cats"],
-       ["camper", "high art"]);
+       ['my car', 'bad cats'],
+       ['camper', 'high art']);
 
 verify(/.../,
-       ["pop culture", "mad props"],
-       ["plop"]);
+       ['pop culture', 'mad props'],
+       ['plop']);
 
 verify(/.../,
-       ["ferret", "ferry", "ferrari"],
-       ["ferrum", "transfer A"]);
+       ['ferret', 'ferry', 'ferrari'],
+       ['ferrum', 'transfer A']);
 
 verify(/.../,
-       ["how delicious", "spacious room"],
-       ["ruinous", "consciousness"]);
+       ['how delicious', 'spacious room'],
+       ['ruinous', 'consciousness']);
 
 verify(/.../,
-       ["bad punctuation ."],
-       ["escape the dot"]);
+       ['bad punctuation .'],
+       ['escape the dot']);
 
 verify(/.../,
-       ["hottentottententen"],
-       ["no", "hotten totten tenten"]);
+       ['hottentottententen'],
+       ['no', 'hotten totten tenten']);
 
 verify(/.../,
-       ["red platypus", "wobbling nest"],
-       ["earth bed", "learning ape"]);
+       ['red platypus', 'wobbling nest'],
+       ['earth bed', 'learning ape']);
 
-
+/**
+ * verify
+ * @param {regexp} regexp
+ * @param {boolean} yes
+ * @param {boolean} no
+ */
 function verify(regexp, yes, no) {
   // Ignore unfinished exercises
-  if (regexp.source == "...") return;
+  if (regexp.source == '...') return;
   yes.forEach(function(s) {
     if (!regexp.test(s))
-      console.log("Failure to match '" + s + "'");
+      console.log('Failure to match "' + s + '"');
   });
   no.forEach(function(s) {
     if (regexp.test(s))
-      console.log("Unexpected match for '" + s + "'");
+      console.log('Unexpected match for "' + s + '"');
   });
 }
