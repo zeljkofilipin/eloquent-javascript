0a91fb5def39298abe0c5aa6e3dfb4efef4c85b3
Željko Filipin <zeljko.filipin@gmail.com>
2017-02-24 14:08:17 +0100

    9: Quoting style, solution

--- exercises/9/quoting.js
+++ exercises/9/quoting.js
@@ -3,5 +3,5 @@
 // eslint-disable-next-line quotes
 let text = "'I'm the cook,' he said, 'it's my job.'";
 // Change this call.
-console.log(text.replace(/A/g, 'B'));
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
 // → "I'm the cook," he said, "it's my job."
