2a1ea2e1cfc7ba21089f0d7c892ff86531635f59
Željko Filipin <zeljko.filipin@gmail.com>
2017-02-24 12:39:05 +0100

    9: Quoting style, eslinting

--- exercises/9/quoting.js
+++ exercises/9/quoting.js
@@ -1,6 +1,7 @@
 'use strict';
 // from http://eloquentjavascript.net/code/#9.2
-var text = "'I'm the cook,' he said, 'it's my job.'";
+// eslint-disable-next-line quotes
+let text = "'I'm the cook,' he said, 'it's my job.'";
 // Change this call.
-console.log(text.replace(/A/g, "B"));
+console.log(text.replace(/A/g, 'B'));
 // → "I'm the cook," he said, "it's my job."
