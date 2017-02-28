1f524be715281dfbd88b46ca27ba7d6627883ec8
Željko Filipin <zeljko.filipin@gmail.com>
2017-02-28 12:30:17 +0100

    10: Month names, month()

--- exercises/10/months.js
+++ exercises/10/months.js
@@ -1,6 +1,18 @@
 'use strict';
 // from http://eloquentjavascript.net/code/#10.1
 // Your code here.
+let month = function() {
+	let names = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+		'August', 'September', 'October', 'November', 'December'];
+	return {
+		name: function(number) {
+			return names[number];
+		},
+		number: function(name) {
+			return names.indexOf(name);
+		},
+	};
+}();
 
 console.log(month.name(2));
 // → March
