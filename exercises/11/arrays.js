d4f2ba67fb898fff3175750c94cd652434610b17
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-01 11:37:53 +0100

    11: Arrays, eslinting

--- exercises/11/arrays.js
+++ exercises/11/arrays.js
@@ -2,18 +2,18 @@
 // from http://eloquentjavascript.net/code/#11.1
 // Modify these definitions...
 
-topEnv["array"] = "...";
+topEnv['array'] = '...';
 
-topEnv["length"] = "...";
+topEnv['length'] = '...';
 
-topEnv["element"] = "...";
+topEnv['element'] = '...';
 
-run("do(define(sum, fun(array,",
-    "     do(define(i, 0),",
-    "        define(sum, 0),",
-    "        while(<(i, length(array)),",
-    "          do(define(sum, +(sum, element(array, i))),",
-    "             define(i, +(i, 1)))),",
-    "        sum))),",
-    "   print(sum(array(1, 2, 3))))");
+run('do(define(sum, fun(array,',
+    '     do(define(i, 0),',
+    '        define(sum, 0),',
+    '        while(<(i, length(array)),',
+    '          do(define(sum, +(sum, element(array, i))),',
+    '             define(i, +(i, 1)))),',
+    '        sum))),',
+    '   print(sum(array(1, 2, 3))))');
 // → 6
