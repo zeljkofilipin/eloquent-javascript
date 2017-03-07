8c376d0501bfa1f7453615a5e69f3310402c8dac
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-07 10:00:32 +0100

    11.4 Fixing scope, eslinting

--- exercises/11/scope.js
+++ exercises/11/scope.js
@@ -3,14 +3,14 @@ const {run, specialForms} = require('./code/11_language');
 
 // from http://eloquentjavascript.net/code/#11.4
 
-specialForms["set"] = function(args, env) {
+specialForms['set'] = function(args, env) {
   // Your code here.
 };
 
-run("do(define(x, 4),",
-    "   define(setx, fun(val, set(x, val))),",
-    "   setx(50),",
-    "   print(x))");
+run('do(define(x, 4),',
+    '   define(setx, fun(val, set(x, val))),',
+    '   setx(50),',
+    '   print(x))');
 // → 50
-run("set(quux, true)");
+run('set(quux, true)');
 // → Some kind of ReferenceError
