a8205ad53fbe9db3a8c2d70166a46d8e105563cf
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-10 13:33:04 +0100

    13.1 Build a table, moved javascript code to separate file

---
+++ exercises/13/table.js
@@ -0,0 +1,34 @@
+'use strict';
+/**
+ * verify
+ * @param {object} data
+ * @return {object} table
+ */
+function buildTable(data) {
+	let table = document.createElement('table');
+
+	let tr = document.createElement('tr');
+	let keys = Object.keys(data[0]);
+	keys.forEach(function(key) {
+		let th = document.createElement('th');
+		th.textContent = key;
+		tr.appendChild(th);
+	});
+	table.appendChild(tr);
+
+	data.forEach(function(mountain) {
+		let tr = document.createElement('tr');
+		keys.forEach(function(key) {
+			let td = document.createElement('td');
+			td.textContent = mountain[key];
+			if (typeof mountain[key] == 'number')
+				td.style.textAlign = 'right';
+			tr.appendChild(td);
+		});
+	table.appendChild(tr);
+	});
+
+	return table;
+}
+if (typeof module != 'undefined' && module.exports)
+  module.exports = {buildTable};
