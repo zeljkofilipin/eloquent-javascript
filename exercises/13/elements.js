2df34d8f47d685760d1b4f483bed481df6cfa833
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-13 16:29:50 +0100

    13.2 Elements by tag name, solution
    From the book. I had something similar, but not exploring child nodes.

--- exercises/13/elements.js
+++ exercises/13/elements.js
@@ -6,8 +6,23 @@
  * @return {object}
  */
 function byTagName(node, tagName) {
-	return true;
+  var found = [];
+  tagName = tagName.toUpperCase();
+
+  function explore(node) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      var child = node.childNodes[i];
+      if (child.nodeType == document.ELEMENT_NODE) {
+        if (child.nodeName == tagName)
+          found.push(child);
+        explore(child);
+      }
+    }
+  }
+
+  explore(node);
+  return found;
 }
 
 if (typeof module != 'undefined' && module.exports)
-  module.exports = {byTagName};
+	module.exports = {byTagName};
