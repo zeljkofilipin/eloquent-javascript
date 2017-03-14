f1b46c03e0deaed591841bcc1375c8f08593445b
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-14 12:04:14 +0100

    13.3 The cat’s hat, solution

--- exercises/13/cat.js
+++ exercises/13/cat.js
@@ -1,3 +1,18 @@
 'use strict';
+
+/**
+ * step
+ * @param {object} timestamp
+ */
+function step(timestamp) {
+  if (!start) start = timestamp;
+  let progress = timestamp - start;
+  cat.style.left = Math.min(progress / 10, 200) + 'px';
+  hat.style.left = 200 - Math.min(progress / 10, 200) + 'px';
+  if (progress < 2000) {
+    window.requestAnimationFrame(step);
+  }
+}
+
 if (typeof module != 'undefined' && module.exports)
-  module.exports = {};
+  module.exports = {step};
