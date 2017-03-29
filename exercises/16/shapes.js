28103aaa8afa3e6a42e542e722896f090791f8f1
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-29 11:59:37 +0200

    16.1 Shapes, use function arguments

--- exercises/16/shapes.js
+++ exercises/16/shapes.js
@@ -3,28 +3,28 @@ let cx = document.querySelector('canvas').getContext('2d');
 
 (function trapezoid(x, y) {
 	cx.beginPath();
-	cx.moveTo(25, 0);
-	cx.lineTo(50, 0);
-	cx.lineTo(75, 25);
-	cx.lineTo(0, 25);
-	cx.lineTo(25, 0);
+	cx.moveTo(x + 25, y);
+	cx.lineTo(x + 50, y);
+	cx.lineTo(x + 75, y + 25);
+	cx.lineTo(x, y + 25);
+	cx.lineTo(x + 25, y);
 	cx.stroke();
-	cx.save();
-})();
+})(0, 0);
 
-(function redDiamond(x, y) {
+(function redDiamond(x, y, width, height) {
+	cx.save();
 	cx.rotate(Math.PI/4);
 	cx.fillStyle = 'red';
-	cx.fillRect(100, 25, 50, 50);
+	cx.fillRect(x, y, width, height);
 	cx.restore();
-})();
+})(100, -100, 50, 50);
 
-(function zigzaggingLines(x, y) {
+(function zigzaggingLines(x, y, step) {
 	cx.beginPath();
-	cx.moveTo(100, 0);
-	for (let i = 5; i < 50; i += 10) {
-	cx.lineTo(150, i);
-	cx.lineTo(100, i+5);
+	cx.moveTo(x, y);
+	for (let i = 5; i < 50; i += step) {
+	cx.lineTo(x + 50, i);
+	cx.lineTo(x, i+5);
 	}
 	cx.stroke();
-})();
+})(200, 0, 10);
