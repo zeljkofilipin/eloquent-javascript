fef9aa8cd3654b5eb2c7da5eb89cd1905d69e129
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-29 11:48:41 +0200

    16.1 Shapes, introduce functions

--- exercises/16/shapes.js
+++ exercises/16/shapes.js
@@ -1,27 +1,30 @@
 'use strict';
 let cx = document.querySelector('canvas').getContext('2d');
 
-// trapezoid
-cx.beginPath();
-cx.moveTo(25, 0);
-cx.lineTo(50, 0);
-cx.lineTo(75, 25);
-cx.lineTo(0, 25);
-cx.lineTo(25, 0);
-cx.stroke();
-cx.save();
+(function trapezoid(x, y) {
+	cx.beginPath();
+	cx.moveTo(25, 0);
+	cx.lineTo(50, 0);
+	cx.lineTo(75, 25);
+	cx.lineTo(0, 25);
+	cx.lineTo(25, 0);
+	cx.stroke();
+	cx.save();
+})();
 
-// red diamond
-cx.rotate(Math.PI/4);
-cx.fillStyle = 'red';
-cx.fillRect(100, 25, 50, 50);
-cx.restore();
+(function redDiamond(x, y) {
+	cx.rotate(Math.PI/4);
+	cx.fillStyle = 'red';
+	cx.fillRect(100, 25, 50, 50);
+	cx.restore();
+})();
 
-// zigzagging line
-cx.beginPath();
-cx.moveTo(100, 0);
-for (let i = 5; i < 50; i += 10) {
-cx.lineTo(150, i);
-cx.lineTo(100, i+5);
-}
-cx.stroke();
+(function zigzaggingLines(x, y) {
+	cx.beginPath();
+	cx.moveTo(100, 0);
+	for (let i = 5; i < 50; i += 10) {
+	cx.lineTo(150, i);
+	cx.lineTo(100, i+5);
+	}
+	cx.stroke();
+})();
