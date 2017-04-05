99342c6c507e0506d662165bc802f4af47ec42e4
Željko Filipin <zeljko.filipin@gmail.com>
2017-04-05 11:45:21 +0200

    17.2 Waiting for multiple promises, eslinting

--- exercises/17/promises.js
+++ exercises/17/promises.js
@@ -1,6 +1,11 @@
 // from http://eloquentjavascript.net/code/#17.2
 'use strict';
 
+/**
+ * all
+ * @param {object} promises
+ * @return {object}
+ */
 function all(promises) {
   return new Promise(function(success, fail) {
     // Your code here.
@@ -9,25 +14,36 @@ function all(promises) {
 
 // Test code.
 all([]).then(function(array) {
-  console.log("This should be []:", array);
+  console.log('This should be []:', array);
 });
+/**
+ * soon
+ * @param {object} val
+ * @return {object}
+ */
 function soon(val) {
   return new Promise(function(success) {
-    setTimeout(function() { success(val); },
+    setTimeout(function() {
+ success(val);
+},
                Math.random() * 500);
   });
 }
 all([soon(1), soon(2), soon(3)]).then(function(array) {
-  console.log("This should be [1, 2, 3]:", array);
+  console.log('This should be [1, 2, 3]:', array);
 });
+/**
+ * fail
+ * @return {object}
+ */
 function fail() {
   return new Promise(function(success, fail) {
-    fail(new Error("boom"));
+    fail(new Error('boom'));
   });
 }
 all([soon(1), fail(), soon(3)]).then(function(array) {
-  console.log("We should not get here");
+  console.log('We should not get here');
 }, function(error) {
-  if (error.message != "boom")
-    console.log("Unexpected failure:", error);
+  if (error.message != 'boom')
+    console.log('Unexpected failure:', error);
 });
