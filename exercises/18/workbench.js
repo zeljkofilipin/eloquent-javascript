46a804a72918f7f3b255c2436e5aec7ce019398d
Željko Filipin <zeljko.filipin@gmail.com>
2017-04-07 12:26:41 +0200

    18.1 A JavaScript workbench, setup

---
+++ exercises/18/workbench.js
@@ -0,0 +1,11 @@
+// from http://eloquentjavascript.net/code/#18.1
+<!doctype html>
+<script src="code/promise.js"></script>
+
+<textarea id="code">return "hi";</textarea>
+<button id="button">Run</button>
+<pre id="output"></pre>
+
+<script>
+  // Your code here.
+</script>
