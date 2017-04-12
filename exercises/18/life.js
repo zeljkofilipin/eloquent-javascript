68a7282105502a396d6523a57a8c14a2b3bbaf46
Željko Filipin <zeljko.filipin@gmail.com>
2017-04-12 15:20:42 +0200

    18.3 Conway’s Game of Life, eslinting

--- exercises/18/life.js
+++ exercises/18/life.js
@@ -1,17 +1,17 @@
 // the initial elements
-var grid = document.querySelector('#grid');
-var next = document.querySelector('#next');
+let grid = document.querySelector('#grid');
+let next = document.querySelector('#next');
 
 // size of the grid
-var x = 3;
-var y = 3;
+let x = 3;
+let y = 3;
 
 // create grid
-for (var i = 0; i < y; i++) {
-	var div = document.createElement('div');
+for (let i = 0; i < y; i++) {
+	let div = document.createElement('div');
 	grid.appendChild(div);
-	for (var j = 0; j < x; j++) {
-		var checkbox = document.createElement('input');
+	for (let j = 0; j < x; j++) {
+		let checkbox = document.createElement('input');
 		checkbox.type = 'checkbox';
 		checkbox.id = 'id' + i + j;
 		div.appendChild(checkbox);
@@ -20,17 +20,15 @@ for (var i = 0; i < y; i++) {
 
 // next generation
 next.addEventListener('click', function() {
-
 	// is checkbox set?
-	var world = [];
-	for (var i = 0; i < y; i++) {
-		var row = [];
-		for (var j = 0; j < x; j++) {
-			var checkbox = document.querySelector('#id' + i + j);
+	let world = [];
+	for (let i = 0; i < y; i++) {
+		let row = [];
+		for (let j = 0; j < x; j++) {
+			let checkbox = document.querySelector('#id' + i + j);
 			row.push(checkbox.checked);
 		}
 		world.push(row);
 	}
 	console.log(world);
-
 });
