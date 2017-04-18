9b4ffccd8835d008c123c4d65bad6eb002ea1664
Željko Filipin <zeljko.filipin@gmail.com>
2017-04-18 11:51:24 +0200

    18.3 Conway’s Game of Life, getRandomIntInclusive()

--- exercises/18/life.js
+++ exercises/18/life.js
@@ -6,6 +6,13 @@ const next = document.querySelector('#next');
 const x = 3;
 const y = 3;
 
+// from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 /**
  * @param {int} x
  * @param {int} y
